23cb98caf497e0ad503650b33fa5cb4f9455bc98
Leshawn Rice <leshawn.rice@outlook.com>
2022-04-03 14:32:02 -0700

    Added add_devices method to grabaphone model

--- python/models/grabaphone.py
+++ python/models/grabaphone.py
@@ -29,7 +29,12 @@ class GrabaphoneAPI(object):
         pass
 
     def get_manufacturer_by_name(self, manufacturer_name):
-        pass
+        manufacturer = self.engine.get_by_column_value(
+            table_name="manufacturers", column_name="name", column_value=manufacturer_name)
+        try:
+            return manufacturer.pop()
+        except IndexError:
+            return None
 
     def add_manufacturers(self, manufacturers):
         for manufacturer in manufacturers:
@@ -38,3 +43,48 @@ class GrabaphoneAPI(object):
                 self.engine.insert(table_name="manufacturers", values=row_vals)
             except Exception as exc:
                 raise exc
+
+    def add_devices(self, devices):
+        for device in devices:
+            manufacturer = self.get_manufacturer_by_name(
+                device.manufacturer.name)
+            if not manufacturer:
+                print("No Manufacturer!")
+                raise ValueError("No Manufacturer!")
+
+            row_vals = {"name": device.name,
+                        "manufacturer_id": manufacturer.id}
+            try:
+                self.engine.insert(
+                    table_name="devices", values=row_vals)
+            except Exception as exc:
+                raise exc
+
+            device_object = None
+
+            device_rows = self.engine.get_by_column_value(
+                table_name="devices",
+                column_name="name",
+                column_value=device.name
+            )
+
+            if len(device_rows) > 1:
+                for row in device_rows:
+                    if row.manufacturer_id == manufacturer.id:
+                        device_object = row
+                        break
+            elif len(device_rows) == 1:
+                device_object = device_rows.pop()
+            else:
+                raise ValueError("Could not get device!")
+
+            device_id = device_object.id
+            for category, spec_dict in device.specs.items():
+                for key, value in spec_dict.items():
+                    row_vals = {"device_id": device_id,
+                                "category": category, "key": key, "value": value}
+                    try:
+                        self.engine.insert(
+                            table_name="specifications", values=row_vals)
+                    except Exception as exc:
+                        raise exc
