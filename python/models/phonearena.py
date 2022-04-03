9cb1d4cbd0b4b09efa2e59909a7a9f7d6f2340ca
Leshawn Rice <leshawn.rice@outlook.com>
2022-04-03 14:33:15 -0700

    Added parse_device_specs method & fixed parse_manufacturer_devices method in phonearena model

--- python/models/phonearena.py
+++ python/models/phonearena.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 import requests
 from bs4 import BeautifulSoup as bsoup
 from models.tables.device import Device
@@ -6,6 +7,7 @@ from models.tables.manufacturer import Manufacturer
 
 class PhonearenaAPI(object):
     BASE_URL = "https://www.phonearena.com"
+    MAX_PAGES = 5
 
     def __init__(self):
         self.session = requests.Session()
@@ -45,22 +47,52 @@ class PhonearenaAPI(object):
         return manufacturers
 
     def parse_manufacturer_devices(self, manufacturer):
-        endpoint = "/phones/manufacturers/{}".format(manufacturer.name)
-        page_html = self.get(endpoint=endpoint)
-        device_cards = self.find_elements_by_class(
-            page_html, "widget-tilePhoneCard")
         devices = set()
-        for device_card in device_cards:
-            link = self.find_elements_by_tag(
-                device_card, "a").pop().get("href")
-            name = self.find_elements_by_class(
-                device_card, "title").pop().string
-            device = Device(name=name, link=link, manufacturer=manufacturer)
-            devices.add(device)
+        # O(1)
+        for page in range(self.MAX_PAGES):
+            endpoint = f"/phones/manufacturers/{manufacturer.name}/page/{page+1}"
+            page_html = self.get(endpoint=endpoint)
+            device_cards = self.find_elements_by_class(
+                page_html, "widget-tilePhoneCard")
+            # O(n)
+            for device_card in device_cards:
+                link = self.find_elements_by_tag(
+                    device_card, "a").pop().get("href")
+                link = link.split(self.BASE_URL)[1]
+                name = self.find_elements_by_class(
+                    device_card, "title").pop().string
+                device = Device(name=name, link=link,
+                                manufacturer=manufacturer)
+                # O(1)
+                devices.add(device)
         return devices
+
+    def parse_device_specs(self, device):
+        page_html = self.get(endpoint=device.link)
+        specs = {}
+        specs_section = self.find_elements_by_class(
+            page_html, "widgetSpecs").pop()
+        specs_divs = self.find_elements_by_tag(specs_section, "section")
+        for section in specs_divs:
+            category = self.find_elements_by_tag(section, "h3").pop().string
+            category = category.strip() if category else ""
+            specs[category] = {}
+            specs_section = self.find_elements_by_tag(section, "tbody").pop()
+            for spec in self.find_elements_by_tag(specs_section, "tr"):
+                name = self.find_elements_by_tag(spec, "th").pop().string
+                name = name.strip().strip(":") if name else ""
+                value = self.find_elements_by_tag(spec, "td").pop().string
+                value = value.strip() if value else ""
+                specs[category][name] = value
+        device.specs = specs
+
+    def get_devices_specs(self, devices):
+        for device in devices:
+            self.parse_device_specs(device)
 
     def get_devices(self, manufacturer):
         devices = self.parse_manufacturer_devices(manufacturer=manufacturer)
+        self.get_devices_specs(devices)
         return devices
 
     def get_manufacturers(self):
