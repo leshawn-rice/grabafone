e2bf73e0e8ba4f4cdff93d33d3a4b5b3dad7531f
Leshawn Rice <leshawn@uplevelsystems.com>
2022-05-18 12:36:40 -0700

    Formatting in NotFound.js

--- frontend/src/components/errors/NotFound.js
+++ frontend/src/components/errors/NotFound.js
@@ -6,9 +6,12 @@ import '../../styles/NotFound.css';
 const NotFound = () => {
   return (
     <div className="NotFound">
-      <h1 className="NotFound-Content"><span className="secondary">404 </span>: The page you are looking for does not exist.</h1>
+      <h1 className="NotFound-Content">
+        <span className="secondary">404 </span>: The page you are looking for
+        does not exist.
+      </h1>
     </div>
   );
-}
+};
 
 export default NotFound;
